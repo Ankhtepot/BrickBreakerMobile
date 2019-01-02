fd80954e9601c1a3e9dda104e5eb6db392e2bc6c
Petr Zavodny <vessago@napismi.cz>
2019-01-02 20:55:01 +0100

    WIP 2.1.2018

--- Assets/Scripts/ResponsiveElementUI.cs
+++ Assets/Scripts/ResponsiveElementUI.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class ResponsiveElementUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class ResponsiveElementUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler {
 
     [SerializeField] bool isOver = false;
     [SerializeField] UnityEvent MouseOver;
     [SerializeField] UnityEvent MouseLeave;
+    [SerializeField] UnityEvent MouseDown;
+    [SerializeField] UnityEvent MouseUp;
+
+    public void OnPointerDown(PointerEventData eventData) {
+        isOver = true;
+        MouseDown.Invoke();
+    }
 
     public void OnPointerEnter(PointerEventData eventData) {
         isOver = true;
@@ -20,5 +27,10 @@ public class ResponsiveElementUI : MonoBehaviour, IPointerEnterHandler, IPointer
     public void OnPointerExit(PointerEventData eventData) {
         isOver = false;
         MouseLeave.Invoke();
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        isOver = false;
+        MouseUp.Invoke();
     }
 }
